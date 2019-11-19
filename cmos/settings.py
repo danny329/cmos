bc53590b8dd938e68098e14e27d713d9a99711e9
danny329 <danielpaul329@outlook.com>
2019-11-19 20:16:49 +0530

    api work

--- cmos/settings.py
+++ cmos/settings.py
@@ -151,3 +151,14 @@ SWEETIFY_SWEETALERT_LIBRARY = 'sweetalert2'
 CRONJOBS = [
     ('*/5 * * * *', 'order.cron.CheckOrderStatusCron')
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
